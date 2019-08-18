4bbbd17b205a7ddad26aab6e26d3666c0067303f
Ashwin Radhakrishnan <ashwin.radhakrishnan1@aexp.com>
2019-08-18 12:49:33 -0700

    initial commit

---
+++ UserInterface/src/main/java/com/travel/company/assit/UserInterface/UserInterfaceApplication.java
@@ -0,0 +1,14 @@
+package com.travel.company.assit.UserInterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserInterfaceApplication.class, args);
+
+	}
+
+}
